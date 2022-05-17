46efe7c18a991e2cfb5aaf94c13444a8489beab0
Puvanesh07 <puvanesh1964@gmail.com>
2022-05-17 09:31:42 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
+let music = new Audio("./Assets/music.mp3")
+let audioTurn = new Audio("./Assets/ting.mp3")
+let gameover = new Audio("./Assets/gameover.mp3")
 let turn = "X"
 let isgameover = false;
 
